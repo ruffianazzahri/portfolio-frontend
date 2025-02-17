7da35f6f8e40fb162e70a7c9bcef8497d3bde617
Muhammad Ruffian Azzahri <muhammadruffianazzahri@gmail.com>
2025-02-17 15:40:28 +0700

    a

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-// Import ikon yang ingin digunakan
 import {
   faHouse,
   faSuitcase,
@@ -19,15 +16,11 @@ import {
   faBriefcase,
   faStar,
 } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
-// Import Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import "./assets/main.css";
-// Import Bootstrap JS (optional, jika Anda ingin menggunakan fitur JS seperti modal, dropdown, dll.)
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// Tambahkan ikon ke library
 library.add(
   faHouse,
   faSuitcase,
@@ -39,11 +32,9 @@ library.add(
   faEnvelope,
   faPhone,
   faFileAlt,
-  faDiagramProject,
   faBriefcase,
   faGithub,
-  faStar,
-  faCertificate
+  faStar
 );
 
 const app = createApp(App);
